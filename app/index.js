0d098c0f073891d05d18d97461daf7b4c16f746c
John Wiseman <jjwiseman@gmail.com>
2022-10-27 09:04:18 -0700

    Add screenshot-ready logging.

--- app/index.js
+++ app/index.js
@@ -174,6 +174,7 @@ class Previewer {
                         const map = window.map;
                         if (map) {
                             console.log("Map exists: " + map);
+                            console.log("WOO");
                             console.log(map.getCenter());
                             clearInterval(interval);
                             console.log("Installing listener");
