4cd2bdaa4824379cadc67394d1fb0640946a9e09
John Wiseman <jjwiseman@gmail.com>
2022-10-31 14:45:37 -0700

    Updated deploy script for new server.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-ssh bitnami@gpsjam.org << EOF
-  cd /home/bitnami/gpsjam.org
-  git pull
-  npm install
+ssh -Y gpsjam@gpsjam.org << EOF
+  cd ~/gpsjam.org &&
+  git pull &&
+  npm install &&
   pm2 restart app
 EOF
