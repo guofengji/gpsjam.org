4947b3e8257a18a85b2a60036ce3318a3cfcf5e4
John Wiseman <jjwiseman@gmail.com>
2022-11-15 21:09:45 -0800

    Speedup manifest gen by multiprocessing.

--- regen.py
+++ regen.py
@@ -2,10 +2,34 @@ import csv
 import datetime
 import glob
 import json
+from multiprocessing import Pool
 import os.path
 import re
 import statistics
 import sys
+from typing import Optional, Tuple
+
+import tqdm
+
+# Returns either (date_str, num_total, num_bad) or None if the file is empty.
+def process_file(path: str) -> Optional[Tuple[str, int, int]]:
+    num_bad_hexes = 0
+    filename = os.path.basename(path)
+    match = re.match(r"^(\d{4}-\d{2}-\d{2}).*?.csv$", filename)
+    if not match:
+        return None
+    date = match.group(1)
+    with open(path, "r") as f:
+        num_aircraft_hexes = 0
+        lines = f.readlines()[1:]
+        for l in lines:
+            pieces = l.split(",")
+            num_good_aircraft = int(pieces[1])
+            num_bad_aircraft = int(pieces[2])
+            num_aircraft_hexes += num_good_aircraft + num_bad_aircraft
+            if (num_bad_aircraft - 1) / (num_good_aircraft + num_bad_aircraft) >= 0.10:
+                num_bad_hexes += 1
+    return (date, num_aircraft_hexes, num_bad_hexes)
 
 
 def main(args):
@@ -15,31 +39,19 @@ def main(args):
     paths = glob.glob("public/data/*.csv")
     all_num_aircraft_hexes = {}
     all_num_bad_hexes = {}
-    for path in paths:
-        num_bad_hexes = 0
-        filename = os.path.basename(path)
-        match = re.match(r"^(\d{4}-\d{2}-\d{2}).*?.csv$", filename)
-        if not match:
-            continue
-        date = match.group(1)
-        dates.append(date)
-        with open(path, "r") as f:
-            num_aircraft_hexes = 0
-            lines = f.readlines()[1:]
-            for l in lines:
-                pieces = l.split(",")
-                num_good_aircraft = int(pieces[1])
-                num_bad_aircraft = int(pieces[2])
-                num_aircraft_hexes += num_good_aircraft + num_bad_aircraft
-                if (num_bad_aircraft - 1) / (num_good_aircraft + num_bad_aircraft) >= 0.10:
-                    num_bad_hexes += 1
-        all_num_aircraft_hexes[date] = num_aircraft_hexes
-        all_num_bad_hexes[date] = num_bad_hexes
+    with Pool() as pool:
+        for result in list(tqdm.tqdm(pool.imap(process_file, paths), total=len(paths))):
+            if result is None:
+                continue
+            date, num_aircraft_hexes, num_bad_hexes = result
+            dates.append(date)
+            all_num_aircraft_hexes[date] = num_aircraft_hexes
+            all_num_bad_hexes[date] = num_bad_hexes
     mean = statistics.mean(all_num_aircraft_hexes.values())
     std_dev = statistics.stdev(all_num_aircraft_hexes.values())
     print("Mean: {}".format(mean))
     print("Std Dev: {}".format(std_dev))
-    print('Suspect dates:')
+    print("Suspect dates:")
     for date in all_num_aircraft_hexes:
         if abs(all_num_aircraft_hexes[date] - mean) > 2 * std_dev:
             print("{}: {}".format(date, all_num_aircraft_hexes[date]))
@@ -53,8 +65,10 @@ def main(args):
             writer.writerow(
                 {
                     "date": date,
-                    "suspect": json.dumps(abs(all_num_aircraft_hexes[date] - mean) > 2 * std_dev),
-                    "num_bad_aircraft_hexes": all_num_bad_hexes[date]
+                    "suspect": json.dumps(
+                        abs(all_num_aircraft_hexes[date] - mean) > 2 * std_dev
+                    ),
+                    "num_bad_aircraft_hexes": all_num_bad_hexes[date],
                 }
             )
 
